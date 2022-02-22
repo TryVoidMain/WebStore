9d86d32e473879bdcae6eb17559945bbacacd131
Kosnstantin <tryvoidmain@gmail.com>
2022-02-22 21:00:04 +0300

    Работа с представлением

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -1,9 +1,15 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Models;
 
 namespace WebStore.Controllers
 {
     public class HomeController : Controller
     {
+        private static readonly List<Employee> _Employees = new()
+        { 
+            new Employee { Id=1, LastName = "Иванов", FirstName = "Vasya", Patronymic="Vasilyevich"}
+        }
+
         public readonly IConfiguration _Configuration;
         public HomeController(IConfiguration Configuration)
         {
@@ -22,6 +28,11 @@ namespace WebStore.Controllers
         public IActionResult ConfigStr()
         {
             return Content($"content {_Configuration["ServerGreetings"]}");
+        }
+
+        public IActionResult Employees()
+        {
+            return View(_Employees);
         }
     }
 }
