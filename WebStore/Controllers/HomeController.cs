5c025808ffcea268ca320c31e243e568c87ba528
Kosnstantin <tryvoidmain@gmail.com>
2022-02-22 20:45:50 +0300

    Первое представление

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -4,14 +4,24 @@ namespace WebStore.Controllers
 {
     public class HomeController : Controller
     {
+        public readonly IConfiguration _Configuration;
+        public HomeController(IConfiguration Configuration)
+        {
+            _Configuration = Configuration;
+        }
         public IActionResult Index()
         {
-            return Content("Hello from controller");
+            return View();
         }
 
         public IActionResult ContentString(string Id = "-id-")
         {
             return Content($"content: {Id}");
+        }
+
+        public IActionResult ConfigStr()
+        {
+            return Content($"content {_Configuration["ServerGreetings"]}");
         }
     }
 }
