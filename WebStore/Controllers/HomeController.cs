e3947ddf141d88e054f9a1918860f0146de8bd7e
Kosnstantin <tryvoidmain@gmail.com>
2022-02-22 22:30:57 +0300

    Добавил двух сотрудников поправил орф ошибку

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -7,7 +7,10 @@ namespace WebStore.Controllers
     {
         private static readonly List<Employee> _Employees = new()
         {
-            new Employee { Id = 1, LastName = "Иванов", FirstName = "Vasya", Patronymic = "Vasilyevich" }
+            new Employee { Id = 1, LastName = "Иванов", FirstName = "Vasya", Patronymic = "Vasilyevich", Age = 21 },
+            new Employee { Id = 2, LastName = "Петров", FirstName = "Альтаир", Patronymic= "Андреевич", Age = 29},
+            new Employee { Id = 3, LastName = "Васечкин", FirstName = "Агрегат", Patronymic = "Рашидович", Age = 32}
+
         };
 
         public readonly IConfiguration _Configuration;
