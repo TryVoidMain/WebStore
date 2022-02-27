70ba2a314d8926672499d7df874edabbdc8154cb
Kosnstantin <tryvoidmain@gmail.com>
2022-02-27 19:39:25 +0300

    Генерация ошибок в режиме разработки проверка на запрос статических файлов wwwroot

--- WebStore/Program.cs
+++ WebStore/Program.cs
@@ -5,6 +5,20 @@ builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
+if(app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage(); //Генерация ошибок в режиме разработки
+}
+
+app.UseStaticFiles(); //Проверка на запрос файлов из папки wwwroot, в случае выполнения - всё что ниже пропускается.
+
+
+//Пример ошибки в приложении
+app.MapGet("/throw", () =>
+{
+    throw new ApplicationException("Пример ошибки приложения");
+});
+
 app.UseRouting();
 
 app.MapGet("/greetings", () => app.Configuration["ServerGreetings"]);
