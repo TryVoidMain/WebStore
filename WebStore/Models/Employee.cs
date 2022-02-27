4208d1bd26b4d33c48702ccb447d3a6a16aa2724
Kosnstantin <tryvoidmain@gmail.com>
2022-02-27 23:22:15 +0300

    Информация о сотруднике.

--- WebStore/Models/Employee.cs
+++ WebStore/Models/Employee.cs
@@ -7,5 +7,11 @@
         public string LastName { get; set; }
         public string Patronymic { get; set; }
         public int Age { get; set; }
+
+        public string ShortName
+        {
+            get => $"{FirstName.Substring(0, 1)}. {Patronymic.Substring(0, 1)}. {LastName}";
+            
+        }
     }
 }
